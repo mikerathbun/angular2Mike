e4b38089d627b022bd5e9203a5484a539ae771f4
Mike Rathbun <michael.g.rathbun.ctr@mail.mil>
2016-08-23 15:28:05 -0500

    starting task-icons

---
+++ app/tasks/task-icons.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from '../shared/shared';
+
+@Component({
+    selector: 'pomodoro-task-icone',
+    template: `<img *ngFor="let icon of icons"
+                    src="/app/shared/assets/img/pomodoro.png"
+                    width="{{size}}">`
+
+})
+export default class TaskIconsComponent implements OnInit {
+    @Input() task: Task;
+    @Input() size: number;
+    icons: Object[] = [];
+
+    ngOnInit() {
+        this.icons.length = this.task.pomodorosRequired;
+        this.icons.fill({ name: this.task.name });
+    }
+}
