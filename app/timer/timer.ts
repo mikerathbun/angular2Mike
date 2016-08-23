20a579fdddf5f0a3b02af97ca0e7cad3888a6a78
Mike Rathbun <michael.g.rathbun.ctr@mail.mil>
2016-08-23 15:18:43 -0500

    timer initial import

---
+++ app/timer/timer.ts
@@ -0,0 +1,11 @@
+import TimerWidgetComponent from './timer-widget.component';
+
+const TIMER_DIRECTIVES: any[] = [
+    TimerWidgetComponent
+];
+
+export {
+    TIMER_DIRECTIVES,
+    // Not sure why we need this here.
+    TimerWidgetComponent
+}
