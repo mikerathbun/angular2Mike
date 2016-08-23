89b16945cb3d981df94256b9f68a3a8817eab08c
Mike Rathbun <michael.g.rathbun.ctr@mail.mil>
2016-08-23 14:03:49 -0500

    created new service for tasks

--- app/shared/shared.ts
+++ app/shared/shared.ts
@@ -1,5 +1,18 @@
 import Queueable from './interfaces/queueable';
+import Task from './interfaces/task';
+
+import FormattedTimePipe from './pipes/formatted-time.pipe';
+import QueuedOnlyPipe from './pipes/queued-only.pipe';
+
+const SHARED_PIPES: any[] = [
+    FormattedTimePipe,
+    QueuedOnlyPipe
+];
 
 export {
-    Queueable
+    Queueable,
+    Task,
+
+    FormattedTimePipe,
+    QueuedOnlyPipe
 };
