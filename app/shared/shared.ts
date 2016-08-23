348fb146897b1e91151378c6913f4569e67a70e6
Mike Rathbun <michael.g.rathbun.ctr@mail.mil>
2016-08-23 14:58:04 -0500

    updated shared to include new services

--- app/shared/shared.ts
+++ app/shared/shared.ts
@@ -4,6 +4,9 @@ import Task from './interfaces/task';
 import FormattedTimePipe from './pipes/formatted-time.pipe';
 import QueuedOnlyPipe from './pipes/queued-only.pipe';
 
+import SettingsService from './services/settings.service';
+import TaskService from './services/task.service';
+
 const SHARED_PIPES: any[] = [
     FormattedTimePipe,
     QueuedOnlyPipe
@@ -14,5 +17,8 @@ export {
     Task,
 
     FormattedTimePipe,
-    QueuedOnlyPipe
+    QueuedOnlyPipe,
+
+    SettingsService,
+    TaskService
 };
