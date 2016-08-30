7cdad722dded6117fc3da90131279276c66903ea
Mike Rathbun <michael.g.rathbun.ctr@mail.mil>
2016-08-30 14:47:42 -0500

    working on task editor

--- app/tasks/task-editor.component.ts
+++ app/tasks/task-editor.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import {
+    ROUTER_DIRECTIVES,
+    CanActivate,
+    ComponentInstruction,
+    OnActivate,
+    CanDeactivate,
+    OnDeactivate } from '@angular/router-deprecated';
+import { Title } from '@angular/platform-browser';
+
 
 @Component({
     selector: 'pomodoro-tasks-editor',
     directives: [ROUTER_DIRECTIVES],
+    providers: [Title],
     templateUrl: 'app/tasks/task-editor.component.html'
 })
-export default class TaskEditorComponent {
-    constructor() {}
+@CanActivate((
+    next: ComponentInstruction,
+    prev: ComponentInstruction): boolean => {
+        let passPhrase = prompt('Say the phrase');
+        return (passPhrase == 'open sesame');
+    }
+)
+
+export default class TaskEditorComponent implements OnActivate {
+    constructor(private title: Title) { }
+    routerOnActivate(
+        next: ComponentInstruction,
+        prev: ComponentInstruction
+    ): void {
+        this.title.setTitle('Welcome to the Task Form');
+    }
 }
