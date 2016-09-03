f179c2f0cba1774e7b576eea95b56cf4e89b77e2
Mike Rathbun <mikerathbun@me.com>
2016-09-03 15:50:54 -0500

    added EventEmitter for userIsLoggedIn

--- app/shared/services/authentication.service.ts
+++ app/shared/services/authentication.service.ts
@@ -2,11 +2,37 @@ import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable()
 export default class AuthenticationService {
-    constructor() {}
 
-    login({username, password}): Promise<boolean> {}
+    userIsLoggedIn: EventEmitter<boolean>;
 
-    logout(): Promise<boolean>{}
+    constructor() {
+        this.userIsLoggedIn = new EventEmitter();
+    }
 
-    static isAuthorized(): boolean {}
+    login({username, password}): Promise<boolean> {
+        return new Promise(resolve => {
+            let validCredentials: boolean = false;
+
+            // @Note: In a real scenario this check
+            // should be performed against a web service.
+            if (username === 'jogn.doe@mail.com' && password === 'letmein') {
+                validCredentials = true;
+                window.sessionStorage.setItem('token', 'eyJhbGci0i');
+            }
+            this.userIsLoggedIn.emit(validCredentials);
+            resolve(validCredentials);
+        });
+    }
+
+    logout(): Promise<boolean>{
+        return new Promise(resolve => {
+            window.sessionStorage.removeItem('token');
+            this.userIsLoggedIn.emit(false);
+            resolve(true);
+        });
+    }
+
+    static isAuthorized(): boolean {
+        return !!window.sessionStorage.getItem('token');
+    }
 }
